ee72b0309484f9ff131f59b59a44dea1f6cfd1af
allyssaangell <a.angell2@yahoo.com>
2022-03-12 21:07:34 -0600

    POST

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,19 @@
-// GET route, POST route, DELETE route
 const fs = require("fs");
 const path = require("path");
 const router = require("express").Router();
-// const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
-const { notes } = require("../../db/db");
+//const { notes } = require("../../db/db");
 
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./db/db"));
+  res.sendFile(path.join(__dirname, "../db/db.json"));
 });
 
-router.post("/api/notes", (req, res) => {
-  //   const notes = JSON.parse(fs.readFileSync("./db/db"));
-//  const note = req.body;
-  //   notes.push(newNote);
-  //   fs.writeFileSync("./db/db", JSON.stringify(notes));
-  //   req.body.id = notes.length.toString();
-  //   const note = saveNote(req.body, notes);
- // res.json(note);
-  console.log(req.body);
+router.post("/notes", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  const note = req.body;
+  req.body.id = notes.length.toString();
+  notes.push(note);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  res.json(note);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 // const { notes } = require('./db/db');
-const htmlRoutes = require('./routes/htmlRoutes');
-// const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 
 const app = express();
 var PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+  console.log(`API server now on port ${PORT}!`);
+});
