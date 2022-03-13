1a5e0928286a39226e164fd59bd5c6a858b79761
allyssaangell <a.angell2@yahoo.com>
2022-03-12 20:13:17 -0600

    GET route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,37 +1,23 @@
 // GET route, POST route, DELETE route
-
-// api route from zookeeper
-// const router = require('express').Router();
+const fs = require("fs");
+const path = require("path");
+const router = require("express").Router();
 // const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
-// const { animals } = require('../../data/animals');
+const { notes } = require("../../db/db");
 
-// router.get('/animals', (req, res) => {
-//     let results = animals;
-//     if (req.query) {
-//         results = filterByQuery(req.query, results);
-//     }
-//     res.json(results);
-// });
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db"));
+});
 
-// router.get('/animals/:id', (req, res) => {
-//     const result = findById(req.params.id, animals);
-//   //  res.json(result);
-//     if (result) {
-//       res.json(result);
-//     } else {
-//       res.send(404);
-//     }
-//   });
+router.post("/api/notes", (req, res) => {
+  //   const notes = JSON.parse(fs.readFileSync("./db/db"));
+//  const note = req.body;
+  //   notes.push(newNote);
+  //   fs.writeFileSync("./db/db", JSON.stringify(notes));
+  //   req.body.id = notes.length.toString();
+  //   const note = saveNote(req.body, notes);
+ // res.json(note);
+  console.log(req.body);
+});
 
-//   router.post('/animals', (req, res) => {
-//     // set id based on what the next index of the array will be
-//     req.body.id = animals.length.toString();
-//     if (!validateAnimal(req.body)) {
-//       res.status(400).send('The animal is not properly formatted.');
-//     } else {
-//       const animal = createNewAnimal(req.body, animals);
-//       res.json(animal);
-//     }
-// });
-
-// module.exports = router;
+module.exports = router;
